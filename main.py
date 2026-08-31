3641a2021e5fe913f8f7daaa7053f165f56d0417
Eadric Chen <735322470@qq.com>
2026-08-31 16:23:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ app_secret = "bd0a64c5716f7478ade3c235ba5f9c73"
 # 微信公众号的user_id,多个用;（分号）隔开
 user_ids = "ollgl3OQx5INiRKhdovFPsJmD9GQ"
 # 白天模板id
-template_id_day = "F3XZTFv_Q34c288DT6NV6qnvs7WZkrBg-0aQwGL-Zlg"
+template_id_day = "g7SNJwa3qk6v2xvPQYvMiKjpFnMgECYj6TWbizN4iMM"
 # 晚上模板id
-template_id_night = "F3XZTFv_Q34c288DT6NV6qnvs7WZkrBg-0aQwGL-Zlg"
+template_id_night = "g7SNJwa3qk6v2xvPQYvMiKjpFnMgECYj6TWbizN4iMM"
 # 呢称
-name = '\U0001F34E'+'宝宝'
+name = '宝宝'
 # 城市
 city = '汕尾市'
 
