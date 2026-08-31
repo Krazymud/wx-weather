c07776faa68ca021be4be5430510f869df052153
Eadric Chen <735322470@qq.com>
2026-08-31 17:02:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ app_secret = "bd0a64c5716f7478ade3c235ba5f9c73"
 # 微信公众号的user_id,多个用;（分号）隔开
 user_ids = "ollgl3OQx5INiRKhdovFPsJmD9GQ"
 # 白天模板id
-template_id_day = "lVE0caMOKtpX1ewQhhCE1XwPKyTGbtimeg4ylGaCXAc"
+template_id_day = "mjBWx7P9LKbeuFzBlwld4QbGrNOD66pnji2ZXLVg6WM"
 # 晚上模板id
-template_id_night = "lVE0caMOKtpX1ewQhhCE1XwPKyTGbtimeg4ylGaCXAc"
+template_id_night = "mjBWx7P9LKbeuFzBlwld4QbGrNOD66pnji2ZXLVg6WM"
 # 呢称
 name = '宝宝'
 # 城市
