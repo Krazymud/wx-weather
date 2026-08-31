dc67d8f3b0e42f3d6bdd045c0adec1fa3be45ecf
Eadric Chen <735322470@qq.com>
2026-08-31 11:52:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ params = {
 }
 print(params)
 # 根据城市名查找地理位置
-url = "https://n93jpfeh9u.re.qweatherapi.com/geo/v2/city/lookup"
+url = "https://m96mthd8c3.re.qweatherapi.com/geo/v2/city/lookup"
 
 resp = requests.get(url, params=params, headers=headers)
 if resp.status_code == 200:
